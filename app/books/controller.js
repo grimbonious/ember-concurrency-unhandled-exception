3843707bb589109de9de1840e8678a89d69bbb7b
Graham Towse <grimbonious@hey.computer>
2019-03-03 16:55:20 +1100

    Add ec error event handler

--- app/books/controller.js
+++ app/books/controller.js
@@ -1,12 +1,18 @@
 import Controller from '@ember/controller';
 import { task } from 'ember-concurrency';
 import { inject as service } from '@ember/service';
+// import RSVP from 'rsvp'
+import Evented, { on } from '@ember/object/evented'
 
-export default Controller.extend({
+export default Controller.extend(Evented, {
   ajax: service(),
 
   createBooksTask: task(function* () {
     yield this.ajax.post('/api/books');
+  }).evented(),
+
+  createBooksTaskErrored: on('createBooksTask:errored', function (taskInstance, error) {
+    console.log(`Task createBooksTask: AJAX failed because of '${error.message}'`);
   }),
 
 
